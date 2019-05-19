097c0c16bf05fbc69386acd82d4f8a22900f8cd1
prashant_yadav <prashantsingh.yadav@sadhan.co.in>
2019-05-19 14:22:02 +0530

    fetching data using axios

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 // making this component class based so that we can fetch data from api
 class AlbumList extends Component {
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response, 'response'))
+  }
   render() {
+    console.log('render');
     return (
       <View>
         <Text>Album List!!!</Text>
